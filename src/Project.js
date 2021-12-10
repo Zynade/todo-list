ccccccad8bbc390d3a5fe96b9f52e733ce1e3411
Atharv <atharvgoel03@gmail.com>
2021-12-10 12:38:02 +0300

    added a Project class

---
+++ src/Project.js
@@ -0,0 +1,36 @@
+class Project {
+    constructor(projectName) {
+        this.projectName = projectName;
+        this.tasks = [];
+    }
+
+    getProjectName() {
+        return this.projectName;
+    }
+
+    setProjectName(name) {
+        this.projectName = name;
+    }
+
+    getTasks() {
+        return this.tasks;
+    }
+
+    setTasks(tasks) {
+        this.tasks = tasks;
+    }
+
+    getTask(taskName) {
+        return this.tasks.find((task) => task.getName() === taskName);
+    }
+
+    addTask(newTask) {
+        if (!this.tasks.find((task) => newTask.name === task.getName())) {
+            this.tasks.push(newTask);
+        }
+    }
+
+    removeTask(taskName) {
+        this.tasks = this.tasks.filter((task) => task.getName() !== taskName);
+    }
+}
