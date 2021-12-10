c1c046693c27b0f196db3201eaa2693eac351b63
Atharv <atharvgoel03@gmail.com>
2021-12-10 13:13:30 +0300

    added a todolist class

---
+++ src/TodoList.js
@@ -0,0 +1,25 @@
+class TodoList {
+    constructor() {
+        this.projects = []
+    }
+
+    getProjects() {
+        return this.projects
+    }
+
+    setProjects(project) {
+        this.projects = project
+    }
+
+    getProject(projectName) {
+        return this.projects.find(project => project.getName() === projectName);
+    }
+
+    addProject(project) {
+        this.projects.push(project);
+    }
+
+    removeProject(projectName) {
+        this.projects = this.projects.filter(project => project.getProjectName() !== projectName);
+    }
+}
