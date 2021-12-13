b152430a0c52968ed56b63024b33651edbfddb0b
Atharv <atharvgoel03@gmail.com>
2021-12-13 13:12:14 +0300

    loads tasks

--- src/UI.js
+++ src/UI.js
@@ -7,17 +7,16 @@ let currentProjectName = 'Home';
 export default class UI {
     static addDefaultData() {
         const defaultProject = new Project('Home');
-        // const tasks = [
-        //     new Task('Iron my clothes'),
-        //     new Task('Charge my phone before tonight\'s party'),
-        //     new Task('Withdraw some cash from the ATM')
-        // ];
-        // defaultProject.setTasks(tasks);
+        const tasks = [
+            new Task('Iron my clothes', '2021/10/28'),
+            new Task('Charge my phone before tonight\'s party', '2020/2/21'),
+            new Task('Withdraw some cash from the ATM', '2022/01/01')
+        ];
+        defaultProject.setTasks(tasks);
 
-        defaultProject.addTask(new Task('Iron my clothes'));
-        defaultProject.addTask(new Task('Charge my phone before tonight\'s party'));
-        defaultProject.addTask(new Task('Withdraw some cash from the ATM'));
-        console.log(defaultProject.getTasks());
+        // defaultProject.addTask(new Task('Iron my clothes'));
+        // defaultProject.addTask(new Task('Charge my phone before tonight\'s party'));
+        // defaultProject.addT(new Task('Withdraw some cash from the ATM'));
         Storage.addProject(defaultProject);
     }
 
@@ -75,7 +74,8 @@ export default class UI {
     
     static tabClickHandler(projectName) {
         currentProjectName = projectName;
-        UI.generateMainHeading();
+        // UI.generateMainHeading();
+        UI.initTasksPage();
     }
 
     static addNewProjectButton() {
@@ -135,7 +135,8 @@ export default class UI {
         console.log('Added project:', project.getProjectName());
         currentProjectName = projectName;
         UI.cancelProject();
-        UI.generateMainHeading();
+        // UI.generateMainHeading();
+        UI.initTasksPage();
     }
 
     static cancelProject() {
@@ -179,7 +180,8 @@ export default class UI {
         }
         UI.deleteProject();
         currentProjectName = Storage.getTodoList().getProjects().slice(-1)[0].getProjectName();
-        UI.generateMainHeading();
+        // UI.generateMainHeading();
+        UI.initTasksPage();
     }
 
     static deleteProject() {
@@ -210,5 +212,82 @@ export default class UI {
     static generateTaskItems() {
         const currentProject = Storage.getTodoList().getProject(currentProjectName);
         currentProject.getTasks().forEach(task => UI.addTaskItem(task));
+        UI.addNewTaskButton();
     }
+
+    static hideNewTaskButton() {
+        document.querySelector('#new-task-button').remove();
+    }
+
+    static addNewTaskButton() {
+        const newButton = document.createElement('div');
+        newButton.textContent = '+';
+        newButton.id = 'new-task-button';
+        document.querySelector('.main-panel-content').appendChild(newButton);
+        newButton.addEventListener('click', UI.addNewTask);
+    }
+
+    static addNewTask(e) {
+        UI.hideNewTaskButton();
+
+        const mainContainer = document.querySelector('.main-panel-content')
+        const div = document.createElement('div');
+        div.classList.add('new-task-container');
+        div.innerHTML = `
+        <div class="new-task-popup-inputs">
+            <input type="text" size="40" name="taskName" class="new-task-input-field" id="new-task-input-field">
+            <input type="date" name="taskDate" class="new-task-date-field" id="new-task-date-field" onfocus="blur()" required>
+        </div>
+        <div class="new-task-popup-buttons">
+            <button id="new-task-add-button">Add</button>
+            <button id="new-task-cancel-button">Cancel</button>
+        </div>
+        `
+        mainContainer.appendChild(div);
+
+        document.querySelector('#new-task-add-button').addEventListener('click', UI.addTaskHandler);
+        document.querySelector('#new-task-cancel-button').addEventListener('click', UI.cancelTask);
+    }
+
+    static addTaskHandler() {
+        const addTaskInput = document.querySelector('.new-task-input-field');
+        const addTaskDate = document.querySelector('.new-task-date-field');
+        const taskName = addTaskInput.value;
+        const taskDate = addTaskDate.value;
+
+        if (taskName === '') {
+            alert('Sorry mate, you can\'t have a blank task name.');
+            return;
+        } 
+        if (Storage.getTodoList().getProject(currentProjectName).containsTask(taskName)) {
+            alert('That project already exists!');
+            return;
+        }
+        if (!taskDate) {
+            alert("Please choose a date.");
+            return;
+        }
+
+        const task = new Task(taskName, taskDate);
+        Storage.addTask(currentProjectName, task);
+        console.log('Added task:', task.getName());
+        UI.cancelTask();
+
+    }
+
+    static cancelTask() {
+        UI.closeAddTaskPopups();
+        UI.clearTasksList();
+        UI.generateTaskItems();
+        // UI.addNewTaskButton();
+    }
+
+    static closeAddTaskPopups() {
+        document.querySelector('.new-task-container').remove();
+    }
+
+    static clearTasksList() {
+        document.querySelector('.tasks-container').innerHTML = '';
+    }
+
 };
