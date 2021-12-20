21aa71892e9eeeb66233fc0ea243ba28b6377775
Atharv <atharvgoel03@gmail.com>
2021-12-20 09:41:09 +0300

    minor tweak

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  mode: "production",
+  mode: 'production',
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
